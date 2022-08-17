4bd03b7cd236d4424614198fe1015f45fb69c05b
Nithaj R <nithaj.r@NetCracker.com>
2022-08-17 15:22:56 +0530

    using two loop and inner one reverse iteration

--- 119.pascals-triangle-ii.java
+++ 119.pascals-triangle-ii.java
@@ -8,33 +8,18 @@ import java.util.ArrayList;
 
 // @lc code=start
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        int stored =0;
-        int xx=0;
-        res.add(1);
-        for(int i=1;i<=rowIndex;i++){
-            for(int j=1;j<=i;j++){
-                if(j==i){
-                    res.add(1);
-                }else{
-                    if(j==1){
-                        stored=res.get(j);
-                        res.set(j, stored+res.get(j-1));
-                    }else{
-                        xx=res.get(j);
-                        res.set(j, stored+res.get(j));
-                        stored = xx;
-
-                    }
-                    
-                    
-                }
+    public List<Integer> getRow(int k) {
+        Integer[] arr = new Integer[k + 1];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+        for(int i=1;i<=k;i++){
+            for(int j=i;j>0;j--){
+                arr[j] = arr[j] + arr[j - 1];
             }
             
             
         }
-        return res;
+        return Arrays.asList(arr);
     }
     
 }
