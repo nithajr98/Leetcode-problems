190423eaa87bb89909ed7ceaada1cc99404ffb79
Nithaj R <nithaj.r@NetCracker.com>
2022-08-17 14:08:21 +0530

    little change

--- 643.maximum-average-subarray-i.java
+++ 643.maximum-average-subarray-i.java
@@ -9,17 +9,13 @@ class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int[] presum = new int[nums.length];
         
-        double max=0;
+        double max;
         double curmax=0;int j=0;
 
         int start=0,end=0;
         presum[0]=nums[0];
         for(int i=1;i<nums.length;i++){
-            presum[i]=nums[i]+presum[i-1];
-            curmax = (presum[i]-presum[j])/(i-j);
-            if(curmax>max){
-                max=curmax;
-            }
+            
         }
         return max;
     }
