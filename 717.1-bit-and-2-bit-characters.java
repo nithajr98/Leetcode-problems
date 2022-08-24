dcc40f0e86f2d635c0dd8685a59a63aa08923914
Nithaj R <nithaj.r@NetCracker.com>
2022-08-24 17:14:21 +0530

    simple

--- 717.1-bit-and-2-bit-characters.java
+++ 717.1-bit-and-2-bit-characters.java
@@ -8,6 +8,31 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         
+        int n=bits.length;
+        
+        if(n==0||bits==null){
+            return false;
+        }else if(n==1 && bits[0]==0){
+            return true;
+        }
+
+        for(int i=0;i<n;i++){
+            if(bits[i]==0){
+                if(i==n-1){
+                    return true;
+                }
+                continue;  
+            }else if(bits[i]==1 && bits[i+1]==0){
+                i++;
+            }else if(bits[i]==1 && bits[i+1]==1){
+                i++;
+            }
+        }
+        return false;
+        
+        
+            
+        
     }
 }
 // @lc code=end
